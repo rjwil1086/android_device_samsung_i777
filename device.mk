7e05a2646a32b64cdb2c505dda58d6da01d5f183
Andrew Dodd <atd7@cornell.edu>
2013-11-12 00:10:41 -0500

    i777: NFC: Remove SE-related stuff
    The old SE-related stuff should've been removed ages ago.
    
    Do not enable HCE yet as it's not implemented for NXP chips
    
    Change-Id: Id1b2b62a7a2555d06b2bb59c2f7d8b8bbdd03f24

--- device.mk
+++ device.mk
@@ -65,21 +65,8 @@ packages/apps/Nfc/migrate_nfc.txt:system/etc/updatecmds/migrate_nfc.txt)
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
 
-# NFC EXTRAS add-on API
-PRODUCT_PACKAGES += \
-	com.android.nfc_extras
+#NFC permissions
 PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-	frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-
-# NFCEE access control
-ifeq ($(TARGET_BUILD_VARIANT),user)
-    	NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access.xml
-else
-    	NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access_debug.xml
-endif
-
-PRODUCT_COPY_FILES += \
-    	$(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
+	frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
 
 $(call inherit-product-if-exists, vendor/samsung/i777/i777-vendor.mk)
